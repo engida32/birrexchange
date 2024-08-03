edcc807214c0ca34c259be36e3489470a30ba8b7
engida32 <engida32lishan@gmail.com>
2024-08-04 02:33:32 +0300

    feat: Add Open Graph and Twitter metadata to layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,24 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "BirrExchange",
   description: "Birr Exchange Rate",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://birrexchange.vercel.app/",
+    images: [
+      {
+        url: "https://upload.wikimedia.org/wikipedia/commons/7/71/Flag_of_Ethiopia.svg",
+        width: 800,
+        height: 600,
+        alt: "Birr Exchange Rate",
+      },
+    ],
+    title: "BirrExchange",
+    description: "Birr Exchange Rate",
+  },
+  twitter: {
+    site: "@site",
+  },
 };
 
 export default function RootLayout({
@@ -16,6 +34,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link
+        rel="icon"
+        href="https://upload.wikimedia.org/wikipedia/commons/7/71/Flag_of_Ethiopia.svg"
+      />
       <body className={inter.className}>{children}</body>
     </html>
   );
