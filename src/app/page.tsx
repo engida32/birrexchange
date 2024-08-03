a73f85ff306776d38436c16f5e341fe5d0ed6abc
engida32 <engida32lishan@gmail.com>
2024-08-04 00:37:00 +0300

    feat: add tailwind and shade cn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h2 className="text-2xl text-red-700">Welcome to Birr Exchange Rate</h2>
+      <Button className="my-4">Welcome</Button>
+    </div>
   );
 }
